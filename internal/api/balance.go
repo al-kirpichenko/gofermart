a0c80bec7cf2cac38eb51dd84ac557abf575dd9a
al-kirpichenko <fuluganchik@yandex.ru>
2023-07-10 23:26:25 +0700

    добавлен запрос баланса пользователя

--- internal/api/balance.go
+++ internal/api/balance.go
@@ -1,11 +1,42 @@
 package api
 
 import (
+	"encoding/json"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+
+	"github.com/al-kirpichenko/gofermart/internal/models"
 )
 
 // получение текущего баланса счёта баллов лояльности пользователя
 
-func (s *Server) Balance(c *gin.Context) {
+func (s *Server) Balance(ctx *gin.Context) {
+
+	var (
+		balance models.Balance
+		user    models.User
+	)
+
+	userID, _ := ctx.Get("userID")
+
+	result := s.DB.First(&user, "id = ?", userID)
+
+	if result.Error != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"status": "fail", "message": "User not found"})
+		return
+	}
+
+	balance.Current = user.Balance
+	balance.Withdrawn = user.Withdrawn
+
+	response, err := json.Marshal(balance)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": "json error"})
+		return
+	}
+
+	ctx.Data(http.StatusOK, "application/json", response)
 
 }
--- internal/models/user.go
+++ internal/models/user.go
@@ -6,8 +6,8 @@ import (
 
 type User struct {
 	gorm.Model
-	Login     string `gorm:"size:255;not null;unique" json:"login"`
-	Password  string `gorm:"size:255;not null;unique" json:"password"`
-	Balance   int
-	Withdrawn int
+	Login     string  `gorm:"size:255;not null;unique" json:"login"`
+	Password  string  `gorm:"size:255;not null;unique" json:"password"`
+	Balance   float64 `json:"-"`
+	Withdrawn float64 `json:"-"`
 }
