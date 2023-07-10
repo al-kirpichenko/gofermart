5c58a34bd11ffd994312cbb98b4ecef95aa9fa8a
al-kirpichenko <fuluganchik@yandex.ru>
2023-07-10 22:55:04 +0700

    models.Order: отладка для автотестов

--- internal/models/order.go
+++ internal/models/order.go
@@ -8,7 +8,7 @@ type Order struct {
 	ID        uint      `gorm:"primaryKey" json:"-"`
 	Number    string    `gorm:"not null;unique" json:"number"`
 	Status    string    `json:"status"`
-	Accrual   int       `json:"accrual"`
+	Accrual   float64   `json:"accrual"`
 	CreatedAt time.Time `json:"uploaded_at"`
 	UpdatedAt time.Time `json:"-"`
 	DeletedAt time.Time `json:"-"`
