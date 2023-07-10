088007c7c7317401a1449d2d6e5d0c574d6412ab
al-kirpichenko <fuluganchik@yandex.ru>
2023-07-10 14:46:24 +0700

    models.Order: переписаны теги

--- internal/models/order.go
+++ internal/models/order.go
@@ -2,15 +2,15 @@ package models
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Order struct {
-	gorm.Model
-	Number   string `gorm:"not null;unique"`
-	UserID   uint
-	Status   string
-	Accrual  int
-	Uploaded time.Time
+	ID        uint      `gorm:"primaryKey" json:"-"`
+	CreatedAt time.Time `json:"-"`
+	Number    string    `gorm:"not null;unique" json:"number"`
+	Status    string    `json:"status"`
+	Accrual   int       `json:"accrual"`
+	UpdatedAt time.Time `json:"uploaded_at"`
+	DeletedAt time.Time `json:"-"`
+	UserID    uint      `json:"-"`
 }
