fcc80c7d40c6bc565c3d2722b8aca93df85755b6
al-kirpichenko <fuluganchik@yandex.ru>
2023-07-10 22:21:13 +0700

    models.Order: отладка для автотестов

--- cmd/gophermart/config/config.go
+++ cmd/gophermart/config/config.go
@@ -12,16 +12,27 @@ type Config struct {
 	ServiceAddress string `env:"ACCRUAL_SYSTEM_ADDRESS"`
 }
 
+//const (
+//	DBhost     = "localhost"
+//	DBuser     = "postgres"
+//	DBpassword = "123"
+//	DBname     = "postgres"
+//)
+
 func NewConfig() *Config {
 
 	config := Config{}
 
-	flag.StringVar(&config.ServiceHost, "a", "localhost:8080", "It's a Host")
+	//ps := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+	//	DBhost, DBuser, DBpassword, DBname)
 
+	flag.StringVar(&config.ServiceHost, "a", "localhost:8080", "It's a Host")
+	//flag.StringVar(&config.DatabaseURI, "d", ps, "it's conn string")
 	flag.StringVar(&config.DatabaseURI, "d", "", "it's conn string")
 	flag.StringVar(&config.ServiceAddress, "r", "", "It's a accrual system address")
 
 	flag.Parse()
+
 	err := env.Parse(&config)
 	if err != nil {
 		panic(err)
