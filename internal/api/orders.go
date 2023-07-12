74f5b86e22e72be236f734256c9ca9c4caa0e50b
al-kirpichenko <fuluganchik@yandex.ru>
2023-07-12 22:19:43 +0700

    рефакторинг

--- internal/api/orders.go
+++ internal/api/orders.go
@@ -71,7 +71,7 @@ func (s *Server) AddOrder(ctx *gin.Context) {
 
 		var user models.User
 
-		loyalty, err := accrual.GetLoyalty(order.Number, s.config.ServiceAddress)
+		loyalty, err := accrual.GetLoyalty(order.Number, serviceAddress)
 		if err != nil {
 			s.Logger.Error("No response from the accrual service", zap.Error(err))
 			order.Status = "INVALID"
